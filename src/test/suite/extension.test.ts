8c10435216119042822475a1a6491259430a879c
Darien Pardinas Diaz <dpardinasdiaz@lat.ai>
2024-05-13 17:27:36 -0400

    Get first unit test working

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,33 +1,21 @@
 import * as assert from "assert";
-import * as path from "path";
-import { EXTENSION_ID } from "../../extension";
+import { EXTENSION_ID, server } from "../../extension";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from "vscode";
 import { makeRequest } from "./sendPostRequest";
-// import * as myExtension from '../extension';
-
-async function sleep(ms: number): Promise<void> {
-  return new Promise(
-      (resolve) => setTimeout(resolve, ms));
-}
 
 suite("Extension Test Suite", () => {
-  suiteSetup(async () => {
-    const sampleWorkspace = path.resolve(__dirname, "../../../sampleWorkspace");
-    let uri = vscode.Uri.file(sampleWorkspace);
-    await vscode.commands.executeCommand("vscode.openFolder", uri);
-    await sleep(3000);
-  });
+  suiteSetup(async () => { });
 
-  suiteTeardown(() => {});
+  suiteTeardown(() => { });
 
   test("check can list extensions", async () => {
+    const address = server.address() as any;
     const extensionIds: string[] = (await makeRequest(
       "custom.listInstalledExtensions",
-      []
+      [], address.port
     )) as string[];
     assert(extensionIds.includes(EXTENSION_ID));
-  });
+  }).timeout(5000);
 });
